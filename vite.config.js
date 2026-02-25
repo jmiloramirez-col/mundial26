829d4cb55ab4e610ecf1f51e9121ba233592a568
jmiloramirez-col <jmiloramirez@gmail.com>
2026-02-25 13:26:58 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-```
-
-Guarda con **Ctrl+S** y luego en la terminal escribe los 3 comandos **uno por uno**:
-```
-git add .
-```
-```
-git commit -m "fix vite config"
-```
-```
-git push
